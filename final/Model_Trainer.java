7f9815a8949cefa58f9aa48d2b63b7dacb934abd
Joseph Haaga <haaga.joe@gmail.com>
2016-10-08 15:52:56 -0400

    print timestamp, key and action

---
+++ final/Model_Trainer.java
@@ -0,0 +1,51 @@
+// 1 Data Munging
+// 	1.1	Read txt file
+//  1.2 Generate vectors describing keystrokes
+// 2 Weka Modelling
+//	2.1 Probabilistic Neural Net?
+
+import java.util.Scanner;
+// import java.util.FileReader;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Model_Trainer{
+
+	public static String print(String words){
+		System.out.println(words);
+		return null;
+	}
+
+	public static void main(String args[]){
+		// Read txt file
+		try{
+			String tempLine; // current string while iterating through file
+			BigInteger timestamp; // epoch time the event occured
+			String actionType; // KeyPressed, KeyReleased, or KeyTyped
+			String key; // Key that triggered the event
+			Scanner sn = new Scanner(new FileReader("quick_brown_fox/AnDee/Sun Sep 25 13:35:27 EDT 2016.txt"));
+			tempLine=sn.nextLine();
+			BigInteger firstTimestamp = new BigInteger(tempLine.substring(tempLine.lastIndexOf(" ")+1));
+			while(sn.hasNextLine()){
+				tempLine = sn.nextLine();
+				try{
+					timestamp = new BigInteger(tempLine.substring(tempLine.lastIndexOf(" ")+1));
+					timestamp = timestamp.subtract(firstTimestamp);
+					actionType = tempLine.substring(tempLine.indexOf(" "),tempLine.indexOf(":"));
+					key = tempLine.substring(tempLine.indexOf("(")+1,tempLine.indexOf(")n"));
+					if(!key.contains("no mods")){
+						print(timestamp + " " + key + " " + actionType);
+					}
+				}catch(Exception f){
+					f.printStackTrace();
+				}
+
+			}
+			sn.close(); // safety first, then teamwork
+		}catch(Exception e){
+			e.printStackTrace();
+
+		}
+	}
+
+}
